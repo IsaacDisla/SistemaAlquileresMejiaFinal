1d8d286f917797db31e34d10e500322595ecbb96
IsaacDisla <isaacreynolddislasantana@gmail.com>
2026-05-25 01:09:28 -0400

    Agregué Conexion.cs

---
+++ Capa.Datos/Conexion.cs
@@ -0,0 +1,9 @@
+﻿using System.Data.SqlClient;
+
+namespace Capa.Datos
+{
+    public class Conexion
+    {
+        public static SqlConnection cn = new SqlConnection(
+            "Server=.;Database=Alquiler_VehiculosMJ;Integrated Security=true");
+}
